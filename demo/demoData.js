a9dcba6fd98e91dfef6e32ff81c9c701789c25a6
nachiketha s h upadhya <nachikethashu@gmail.com>
2015-08-23 18:51:35 +0530

    the basic layout

---
+++ demo/demoData.js
@@ -0,0 +1,14 @@
+modelData = [{
+        src: "images/apple.jpg",
+        title: "Page - 1"
+    },{
+        src: "images/lemon.jpg",
+        title: "Page - 2"
+    },{
+        src: "images/orange.jpg",
+        title: "Page - 3"
+    },{
+        src: "images/pear.jpg",
+        title: "Page - 3"
+    }
+];
---
+++ src/flipper.jquery.js
@@ -0,0 +1,11 @@
+(function( $ ) {
+
+    $.fn.flipper = function(elements) {
+        var stage = this;
+        console.log(stage);
+        console.log(elements);
+
+        return this;
+    };
+
+}( jQuery ));
